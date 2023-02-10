13dfa7a9a0902940597740775f3677274efe4fe0
Sillas Bernardo <syllasbernardo@gmail.com>
2023-02-10 13:53:14 -0300

    commit

---
+++ src/index.test.js
@@ -0,0 +1,6 @@
+import sum from "./index";
+import jest from "jest";
+
+test('adds 1 + 2 to equal 3', () => {
+	expect(sum(1, 2).toBe(3))
+})
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+	module: {
+		rules: [
+			// Bundles javascript files
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			// Bundles css and sass files
+			{
+				test: /\.s[ac]ss$/i,
+				use: ["style-loader", "css-loader", "sass-loader"]
+			},
+			// Bundles image files
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource'
+			},
+			// Bundles font files
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource'
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./index.html",
+			filename: "index.html"
+		})
+	],
+	optimization: {
+		minimize: true,
+		minimizer: [new TerserWebpackPlugin()]
+	}
+}
