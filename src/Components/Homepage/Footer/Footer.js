7afc4423accaad72ac2450be65dde90ebd6a4c54
Sillas Bernardo <syllasbernardo@gmail.com>
2023-02-17 11:37:42 -0300

    commit

---
+++ src/Components/Homepage/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+      <span>Copyright 2023</span>
+    </div>
+  );
+};
+
+export default Footer;
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import Header from "../Components/Homepage/Header/Header";
 import Presentation from "../Components/Homepage/Presentation/Presentation";
@@ -6,15 +6,22 @@ import Awards from "../Components/Homepage/Awards/Awards";
 import './Homepage.scss';
 import Reviews from "../Components/Homepage/Reviews/Reviews";
 import About from "../Components/Homepage/About/About";
+import Footer from "../Components/Homepage/Footer/Footer";
+import { MobileViewContext } from "../Components/Context/MobileViewContext";
 
 const Homepage = () => {
+	const isMobile = useContext(MobileViewContext);
+
+	console.log(isMobile)
+
 	return (
 		<div id="homepage-container">
 			<Header />
 			<Presentation />
 			<Awards />
 			<Reviews />
-			<About />
+			{!isMobile && <About />}
+			<Footer />
 		</div>
 	)
 }
