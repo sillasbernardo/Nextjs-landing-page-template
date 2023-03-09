e8b4d9b8e8efd2bdabce1f83ff790e7a94e448c6
sillasbernardo <sillasbernardo@gmail.com>
2023-03-09 07:39:32 -0300

    commit

--- api/cloudinary_api.js
+++ api/cloudinary_api.js
@@ -1,5 +1,14 @@
+/* This is a Cloudinary API setup */
+/* ****************************** */
+/* Check Cloudinary documentation for more informations */
+/* https://cloudinary.com/documentation/cloudinary_get_started */
+
 const cloudinary = require('cloudinary').v2;
 
+// temp
+const dotenv = require('dotenv').config({ path: '../.env' });
+
+// Authentication
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
@@ -7,4 +16,14 @@ cloudinary.config({
 	secure: true
 })
 
-console.log(cloudinary.config())
+const searchImages = () => {
+	cloudinary.search
+		.expression('Presentation')
+		.sort_by('public_id', 'desc')
+		.execute()
+		.then(result => {
+			console.log(result)
+		})
+}
+
+searchImages()
