22fa6694c93c3d366309d09eee351818097cbf75
sillasbernardo <syllasbernardo@gmail.com>
2023-03-16 16:00:25 -0300

    setup env variables and bundles

--- api/Shared/Handlers/ApiErrorHandler.js
+++ api/Shared/Handlers/ApiErrorHandler.js
@@ -4,8 +4,6 @@ const apiErrorHandler = (err, req, res, next) => {
 	if (err instanceof ApiError){
 		res.status(err.code).json(err.message)
 	}
-
-	res.status(500).json('Something went wrong')
 }
 
 module.exports = apiErrorHandler;
